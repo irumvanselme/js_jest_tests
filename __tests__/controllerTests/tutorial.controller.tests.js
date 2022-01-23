e75c922995568fd1c9a358a9b9a339271fa73b93
MikeManzi <manzimike378@gmail.com>
2022-01-23 13:23:04 +0200

    (chore): creating a tutorial success test

--- __tests__/controllerTests/tutorial.controller.tests.js
+++ __tests__/controllerTests/tutorial.controller.tests.js
@@ -1,9 +1,24 @@
-const { Test } = require('supertest');
-const {findAll,update} = require('../../app/controllers/tutorial.controller');
+// const { Test } = require('supertest');
+const request = require('supertest')
+const {create,findAll,update} = require('../../app/controllers/tutorial.controller');
+const app = require('../../app')
 
 
 describe('testing create function',()=>{
     test('Testing if that function exists',()=>{
         expect(findAll).toBeDefined();
+    })
+})
+
+describe('Post endpoints',()=>{
+    it('Testing creating tutorial success', async()=>{
+        const res = await request(app)
+        .post('/api/tutorials/')
+        .send({
+            title: 'Test tutorial',
+            description: 'Test tutorial description',
+            published: true
+        })
+        expect(res.statusCode).toEqual(201)  
     })
 })
--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -20,7 +20,7 @@ exports.create = (req, res) => {
     tutorial
         .save(tutorial)
         .then((data) => {
-            res.send(data);
+            res.status(201).send(data);
         })
         .catch((err) => {
             res.status(500).send({
