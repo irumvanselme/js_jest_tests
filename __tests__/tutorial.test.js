5cba6c0f9c900ea0684646fcd7230f867e8cce6b
irumvanselme <andesanselme@gmail.com>
2022-01-26 10:30:46 +0200

    refactor: make the codes clean

--- __tests__/tutorial.test.js
+++ __tests__/tutorial.test.js
@@ -7,10 +7,11 @@ const { expect } = chai;
 const db = require("../app/models");
 const Tutorial = db.tutorials;
 const app = require("../app");
+const { mongoose } = require("../app/models");
 
 chai.use(http);
 
-describe("Get endpoint", () => {
+describe("Tutorial endpoint", () => {
     let data = [
         {
             _id: "4882200e85yytii999",
@@ -25,26 +26,42 @@ describe("Get endpoint", () => {
             published: true,
         },
     ];
+
     let emptyArr = [];
-    test("should return 200 on sucess", async () => {
+
+    test("GET /api/tutorials --> should return 200 on sucess", async () => {
         jest.spyOn(Tutorial, "find").mockReturnValue(Promise.resolve(data));
         const res = await chai.request(app).get("/api/tutorials");
         expect(res.status).to.equal(200);
+        await mongoose.disconnect();
     });
 
-    test("should return title at index 0 of the body returned", async () => {
+    test("GET /api/tutorials --> should return title at index 0 of the body returned", async () => {
         jest.spyOn(Tutorial, "find").mockReturnValue(Promise.resolve(data));
         const res = await chai.request(app).get("/api/tutorials");
         expect(res.body[0].title).to.equal("react native");
     });
-    test("should return 404 if there is no empty data", async () => {
+    test("GET /api/tutorials --> should return 404 if there is no empty data", async () => {
         jest.spyOn(Tutorial, "find").mockReturnValue(Promise.resolve(emptyArr));
         const res = await chai.request(app).get("/api/tutorials");
         expect(res.status).to.equal(404);
     });
-});
 
-describe("Put endpoint", () => {
+    it("GET /api/tutorials/:id --> should return tutorial by id successfully", async () => {
+        let tutorial = {
+            _id: "4665ytuugi86886000",
+            title: "Test native",
+            description: "mobile development",
+            published: false,
+        };
+        jest.spyOn(Tutorial, "findById").mockReturnValue(
+            Promise.resolve(tutorial)
+        );
+        const response = await request(app).get("/api/tutorials/dSDSAFDSDSDAS");
+        expect(response.statusCode).to.equal(200);
+        expect(response.body.title).to.equal("Test native");
+    });
+
     let updatedTurtorial = {
         _id: "4665ytuugi86886000",
         title: "react native",
@@ -52,7 +69,7 @@ describe("Put endpoint", () => {
         published: false,
     };
 
-    test("should return 201 if the turtorial is updated", async () => {
+    test("PUT /api/tutorials/:id -->should return 201 if the turtorial is updated", async () => {
         jest.spyOn(Tutorial, "findByIdAndUpdate").mockReturnValue(
             Promise.resolve(updatedTurtorial)
         );
@@ -61,7 +78,7 @@ describe("Put endpoint", () => {
             .put("/api/tutorials/:4665ytuugi86886000");
         expect(res.body.message).to.equal("Tutorial was updated successfully.");
     });
-    test("should return 404 if no data was given", async () => {
+    test("PUT /api/tutorials/:id --> should return 404 if no data was given", async () => {
         jest.spyOn(Tutorial, "findByIdAndUpdate").mockReturnValue(
             Promise.resolve(null)
         );
@@ -70,9 +87,8 @@ describe("Put endpoint", () => {
             .put("/api/tutorials/:4665ytuugi86886000");
         expect(res.body.message).to.equal("Not Found");
     });
-});
-describe("Post endpoints", () => {
-    it("should create tutorial successfully", async () => {
+
+    it("POST /api/tutorials --> should create tutorial successfully", async () => {
         jest.spyOn(Tutorial, "create").mockReturnValue(Promise.resolve(true));
 
         const res = await request(app).post("/api/tutorials/").send({
@@ -83,7 +99,7 @@ describe("Post endpoints", () => {
 
         expect(res.statusCode).to.equal(201);
     });
-    it("should not create tutorial if title is missing", async () => {
+    it("POST /api/tutorials --> should not create tutorial if title is missing", async () => {
         const res = await request(app).post("/api/tutorials/").send({
             title: "",
             description: "Test tutorial description",
@@ -92,10 +108,8 @@ describe("Post endpoints", () => {
         expect(res.statusCode).to.equal(400);
         expect(res.body.message).to.equal("Title can not be empty!");
     });
-});
 
-describe("Delete endpoints", () => {
-    it("should delete one tutorial successfully", async () => {
+    it("DELETE /api/tutorials/:id -->should delete one tutorial successfully", async () => {
         jest.spyOn(Tutorial, "findByIdAndRemove").mockReturnValue(
             Promise.resolve(true)
         );
@@ -114,7 +128,7 @@ describe("Delete endpoints", () => {
         );
     });
 
-    it("should delete all tutorials successfully", async () => {
+    it("DELETE /api/tutorials --> should delete all tutorials successfully", async () => {
         jest.spyOn(Tutorial, "deleteMany").mockReturnValue(
             Promise.resolve(true)
         );
@@ -123,22 +137,5 @@ describe("Delete endpoints", () => {
         expect(response.body.message).to.equal(
             "Tutorials were deleted successfully!"
         );
-    });
-});
-
-describe("GET endpoints", () => {
-    it("should return tutorial by id successfully", async () => {
-        let tutorial = {
-            _id: "4665ytuugi86886000",
-            title: "Test native",
-            description: "mobile development",
-            published: false,
-        };
-        jest.spyOn(Tutorial, "findById").mockReturnValue(
-            Promise.resolve(tutorial)
-        );
-        const response = await request(app).get("/api/tutorials/dSDSAFDSDSDAS");
-        expect(response.statusCode).to.equal(200);
-        expect(response.body.title).to.equal("Test native");
     });
 });
